47729fd27a511e4802bc1a52b1b5172c23743dae
Sebastian Cato <seb.cato@gmail.com>
2014-09-10 13:50:00 +0200

    Added more types

--- systm/types.h
+++ systm/types.h
@@ -2,13 +2,23 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-typedef long ssize_t;
+
+typedef void* caddr_t; 
+
+
+typedef unsigned long off_t;
 typedef unsigned long size_t;
+
+typedef long ssize_t;
+
+typedef unsigned int u_int;
 typedef unsigned int pid_t;
 typedef unsigned int uid_t; 
 typedef unsigned int gid_t;
-typedef void* caddr_t; 
-typedef unsigned long off_t;
-typedef unsigned int u_int;
+typedef unsigned int uint32_t;
+
+typedef unsigned short uint16_t;
+
 typedef unsigned char u_char;
+typedef unsigned char uint8_t;
 #endif
